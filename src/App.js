463194ff22247dc4646b8984a3135bfff10f83d3
Mario Campa <mcampa@twitter.com>
2018-11-01 12:50:00 -0700

    Responsive sidebar and animations

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MapComponent from './MapComponent';
 import Sidebar from './Sidebar';
 import { AppProvider } from './context';
-
+import classnames from 'classnames';
+import SidebarOpener from './SidebarOpener';
 import './App.css';
 
 export default function App() {
+  const [open, setOpen] = useState(true);
   return (
     <AppProvider>
       <div className="App">
-        <div className="App-sidebar">
+        <div className={classnames('App-sidebar', open && 'open')}>
           <Sidebar />
+          <SidebarOpener onClick={() => setOpen(!open)} />
         </div>
         <div className="App-main">
           <MapComponent />
